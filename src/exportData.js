e33a191194935102b7f795fb5f4aa47712789a62
Piotr Dubiel <piotrdubiel@icloud.com>
2019-08-29 13:53:02 +0200

    Mozilla: Fix bug with exporting data

--- src/exportData.js
+++ src/exportData.js
@@ -1,8 +1,13 @@
 export default function exportData(text, filename) {
-  var blob = new Blob([text], {type: "text/plain"});
+  var blob = new Blob([text], { type: "text/plain" });
   var url = window.URL.createObjectURL(blob);
   var a = document.createElement("a");
   a.href = url;
+  document.body.appendChild(a);
   a.download = filename;
+  setTimeout(function() {
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }, 100);
   a.click();
 }
